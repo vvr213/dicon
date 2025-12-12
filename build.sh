c0326ad1c2ac9658e0f9326e8c0f9e580fb891c4
vena <vena@venanoMacBook-Pro.local>
2025-12-13 02:26:46 +0900

    fix: 資料通りbuild.shにユーザー作成コードを追記

--- build.sh
+++ build.sh
@@ -10,3 +10,12 @@ python manage.py collectstatic --no-input
 
 #3.データベースのマイグレーション（本番DBにテーブル作成）
 python manage.py migrate
+
+# ▼▼▼ スーパーユーザーの自動作成 (ここを追加) ▼▼▼
+# もし環境変数が設定されていたら実行する
+if [[ -n "$DJANGO_SUPERUSER_USERNAME" ]]; then
+    echo "Creating superuser..."
+    # --noinput: 対話入力をスキップ
+    # || echo ...: すでにユーザーが存在してエラーになっても、無視して次へ進む
+    python manage.py createsuperuser --noinput || echo "Superuser already exists."
+fi
